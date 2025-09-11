447b8545c75f434ace461d4b8ce99909bd5e0375
uw819kt <miya0arima@gmail.com>
2025-09-11 15:13:23 +0900

    fix: ルートページ表示エラー修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
+'use client'
+
+import React from 'react'
 import { Profile } from '@/components/sections/Profile'
-// import { About } from '@/components/sections/About'
-import { Skills } from '@/components/sections/Skills'
-// import { Projects } from '@/components/sections/Projects'
 
 export default function HomePage() {
-  return (
-    <main className="relative z-10">
-      <Profile />
-      {/* <About /> */}
-      <Skills />
-      {/* <Projects /> */}
-    </main>
-  )
+  return <Profile />
 }
